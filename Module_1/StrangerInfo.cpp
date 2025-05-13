b981ce22c1decf5775a544403e7564f9e4946e2f
davidGJohn <david.john@csuglobal.edu>
2025-05-13 18:19:13 -0400

    Finished Implementation

---
+++ Module_1/StrangerInfo.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+// Standard namespace declaration
+using namespace std;
+
+// Main Function
+int main()
+{	
+	// Initializing information
+	string firstName = "Jeffery";
+	string lastName = "Roberts";
+	string address = "2714 Joe Lane";
+	string city = "Burmingham";
+	int zipCode = 27215;
+	
+	cout << "Hello, I am " << firstName << " " << lastName << endl;
+	cout << "I live at " << address << ", " << zipCode << " in " << city << endl;
+	
+
+	return 0;
+}
